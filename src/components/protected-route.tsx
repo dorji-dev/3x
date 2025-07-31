23ce0e3cf82e7fac96465eff82b4daeebe01816c
Dorji Tshering <dorji.react@gmail.com>
2025-07-31 20:23:00 +0600

    feat: restrict app to emails

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,7 +1,9 @@
 import { useRouter } from '@tanstack/react-router'
-import { useConvexAuth } from 'convex/react'
+import { useConvexAuth, useQuery } from 'convex/react'
 import { useEffect, type ReactNode } from 'react'
 import Loader from './ui/loader'
+import { api } from 'convex/_generated/api'
+import { Button } from './ui/button'
 
 interface ProtectedRouteProps {
   children: ReactNode
@@ -10,6 +12,10 @@ interface ProtectedRouteProps {
 export function ProtectedRoute({ children }: ProtectedRouteProps) {
   const router = useRouter()
   const { isAuthenticated, isLoading } = useConvexAuth()
+  const currentUser = useQuery(api.users.getCurrentUser)
+
+  const allowedEmails =
+    (import.meta as any).env.VITE_ALLOWED_EMAILS?.split('|') ?? []
 
   useEffect(() => {
     if (!isLoading && !isAuthenticated) {
@@ -18,6 +24,23 @@ export function ProtectedRoute({ children }: ProtectedRouteProps) {
   }, [isAuthenticated, isLoading, router])
 
   if (isLoading || !isAuthenticated) return <Loader />
+
+  if (!allowedEmails.includes(currentUser?.email ?? '')) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <div className="flex flex-col items-center justify-center space-y-8">
+          <p className="font-bold">You are not allowed to access this app</p>
+          <Button
+            onClick={() => {
+              window.open('https://x.com/DorjiBolt', '_blank')
+            }}
+          >
+            Request Access
+          </Button>
+        </div>
+      </div>
+    )
+  }
 
   return <>{children}</>
 }
