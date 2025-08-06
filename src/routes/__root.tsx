91eb5bf0d24e6a4916f8f8f10c3f73b7019192e2
Dorji Tshering <dorji.react@gmail.com>
2025-08-06 21:57:52 +0600

    feat: update font

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -72,7 +72,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Quantico:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
         <HeadContent />
